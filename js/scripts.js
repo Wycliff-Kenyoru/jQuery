97b3b6d155367ce5fd38dd7be5f4cce42cd66f50
Wycliff-Kenyoru <wycliffkerongo@gmail.com>
2018-07-04 13:11:04 +0300

    jQuery

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("h1").click(function(){
+    alert ("this is a header.");
+  });
+  $("p").click(function(){
+    alert("this is a paragraph.");
+  });
+  $("img").click(function(){
+    alert("this is an img");
+  });
+  $("h1").click(function(){
+    alert("This is a header.");
+    alert("I told you. THIS IS A HEADER");
+  });
+
+});
